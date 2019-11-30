a311b96cb8cad6f0db34009b049a5fd50dfb9bac
Seungki Hong <skhong0831@gmail.com>
2019-12-01 01:09:06 +0900

    up

--- base/embeddings.py
+++ base/embeddings.py
@@ -154,40 +154,40 @@ class PretrainedEmbeddingLoader(object):
 #     utils.log('writing vectors!')
 #     self._write()
 
-  def build(self):
-    utils.log('loading pretrained embeddings from',
-            self.config.pretrained_embeddings_file)
-    for special in SPECIAL_TOKENS:
-      self._add_vector(special)
-    for extra in _EXTRA_WORDS:
-      self._add_vector(extra)
-    with tf.gfile.GFile(self.config.pretrained_embeddings_file, 'r') as f:
-      for i, line in enumerate(f):
-        if i % 10000 == 0:
-          utils.log('on line', i)
- 
-        split = line.decode('utf8').split()
-        w = normalize_word(split[0])
- 
-        try:
-          vec = np.array(map(float, split[1:]), dtype='float32')
-          if vec.size != self.vector_size:
-            utils.log('vector for line', i, 'has size', vec.size, 'so skipping')
-            utils.log(line[:100] + '...')
-            continue
-        except:
-          utils.log('can\'t parse line', i, 'so skipping')
-          utils.log(line[:100] + '...')
-          continue
-        if w not in self.vocabulary:
-          self.vocabulary[w] = len(self.vectors)
-          self.vectors.append(vec)
-    utils.log('writing vectors!')
-    self._write()
-
-  def _write(self):
-    utils.write_cpickle(np.vstack(self.vectors), self.config.word_embeddings)
-    utils.write_cpickle(self.vocabulary, self.config.word_vocabulary)
+#   def build(self):
+#     utils.log('loading pretrained embeddings from',
+#             self.config.pretrained_embeddings_file)
+#     for special in SPECIAL_TOKENS:
+#       self._add_vector(special)
+#     for extra in _EXTRA_WORDS:
+#       self._add_vector(extra)
+#     with tf.gfile.GFile(self.config.pretrained_embeddings_file, 'r') as f:
+#       for i, line in enumerate(f):
+#         if i % 10000 == 0:
+#           utils.log('on line', i)
+#  
+#         split = line.decode('utf8').split()
+#         w = normalize_word(split[0])
+#  
+#         try:
+#           vec = np.array(map(float, split[1:]), dtype='float32')
+#           if vec.size != self.vector_size:
+#             utils.log('vector for line', i, 'has size', vec.size, 'so skipping')
+#             utils.log(line[:100] + '...')
+#             continue
+#         except:
+#           utils.log('can\'t parse line', i, 'so skipping')
+#           utils.log(line[:100] + '...')
+#           continue
+#         if w not in self.vocabulary:
+#           self.vocabulary[w] = len(self.vectors)
+#           self.vectors.append(vec)
+#     utils.log('writing vectors!')
+#     self._write()
+# 
+#   def _write(self):
+#     utils.write_cpickle(np.vstack(self.vectors), self.config.word_embeddings)
+#     utils.write_cpickle(self.vocabulary, self.config.word_vocabulary)
 
 
 def normalize_chars(w):
